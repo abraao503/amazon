6048af1e7842ff2ea31625250d38342666437f59
abraao503 <abraao503@gmail.com>
2024-05-08 13:04:31 -0400

    chore: Update image styling and package dependencies

--- src/components/fullChatModal.tsx
+++ src/components/fullChatModal.tsx
@@ -1,26 +1,35 @@
+import { useEffect, useState } from "react";
 import whatsapp from "../assets/whatsapp.png";
 import ReactLoading from "react-loading";
 
-// type Message =
-//   | {
-//       text: string;
-//       showAction: boolean;
-//     }
-//   | {
-//       text: string;
-//       type: "bot" | "user";
-//     };
+type Message = {
+  text: string;
+  showAction: boolean;
+  type: "bot" | "user";
+};
 
-// const messages: Message[] = [
-//   {
-//     text: "Olá, tudo bem?",
-//     type: "bot",
-//   },
-//   {
-//     text: "Para te ajudar, preciso de algumas informações rápidas 😊",
-//     showAction: true,
-//   },
-// ];
+const allMessages: Message[] = [
+  {
+    text: "Olá, tudo bem?",
+    type: "bot",
+    showAction: false,
+  },
+  {
+    text: "Para te ajudar, preciso de algumas informações rápidas 😊",
+    showAction: true,
+    type: "bot",
+  },
+  {
+    text: "Certo!",
+    type: "bot",
+    showAction: false,
+  },
+  {
+    text: "Qual é o seu nome e sobrenome?",
+    type: "bot",
+    showAction: false,
+  },
+];
 
 type FullChatModalProps = {
   open: boolean;
@@ -62,7 +71,11 @@ function BotMessage({ message }: { message: string }) {
   );
 }
 
-function BotMessageWithAction() {
+type BotMessageWithActionProps = {
+  clickAction: () => void;
+};
+
+function BotMessageWithAction({ clickAction }: BotMessageWithActionProps) {
   return (
     <div className="flex my-[5px]">
       <div className="mr-[15px]">
@@ -73,7 +86,10 @@ function BotMessageWithAction() {
           <p className="text-[15px] p-2 text-[#000000] p-0">
             Para te ajudar, preciso de algumas informações rápidas 😊
           </p>
-          <button className="flex items-center bg-[#0d9f16] rounded-[18px] mt-4 mb-3">
+          <button
+            className="flex items-center bg-[#0d9f16] rounded-[18px] mt-4 mb-3"
+            onClick={clickAction}
+          >
             <span>Claro, vamos lá</span>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -84,7 +100,7 @@ function BotMessageWithAction() {
               viewBox="0 0 16 16"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
               ></path>
             </svg>
@@ -110,6 +126,33 @@ function BotMessageWithAction() {
 }
 
 export default function FullChatModal({ onClose, open }: FullChatModalProps) {
+  const [showLoadingMessage, setShowLoadingMessage] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [currentMessageIndex, setCurrentMessageIndex] = useState(0);
+  const [firstOpen, setFirstOpen] = useState(false);
+
+  useEffect(() => {
+    if (open && !firstOpen) {
+      setFirstOpen(true);
+      setShowLoadingMessage(true);
+    }
+  }, [open, firstOpen]);
+
+  useEffect(() => {
+    if (!showLoadingMessage) return;
+
+    const timer = setTimeout(() => {
+      setShowLoadingMessage(false);
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        allMessages[currentMessageIndex],
+      ]);
+      setCurrentMessageIndex((prevIndex) => prevIndex + 1);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [showLoadingMessage, currentMessageIndex]);
+
   return (
     <div
       className={`bg-[#f7f7f5] h-screen fixed z-50 inset-0 overflow-y-auto ${
@@ -128,18 +171,49 @@ export default function FullChatModal({ onClose, open }: FullChatModalProps) {
             </div>
           </div>
           <div className="border-2 rounded-full">
-            <button type="button" onClick={onClose}>
+            <button
+              type="button"
+              onClick={onClose}
+              className="focus:outline-none"
+            >
               x
             </button>
           </div>
         </div>
         <div className="flex flex-col p-4">
-          <BotMessage message="Olá, tudo bem?" />
-          <BotMessageWithAction />
-          <LoadingMessage />
+          {messages.map((message, index) => {
+            if (message.type === "bot") {
+              if (message.showAction) {
+                return (
+                  <BotMessageWithAction
+                    key={index}
+                    clickAction={() => setShowLoadingMessage(true)}
+                  />
+                );
+              } else {
+                return <BotMessage key={index} message={message.text} />;
+              }
+            }
+          })}
+          {showLoadingMessage && <LoadingMessage />}
         </div>
-        <div className="absolute bottom-0 left-0 p-4 w-full ">
-          <input className="h-[50px] rounded-r-[50px] rounded-l-[50px] border-[#0d9f16] border-[3px] w-full" />
+        <div className="absolute bottom-0 left-0 p-2 w-full">
+          <div className="h-[60px] rounded-r-[60px] rounded-l-[60px] border-[#0d9f16] border-[3px] w-full bg-[#00000] flex items-center pl-[20px] grid gap-x-2 grid-cols-[1fr_60px] bg-white">
+            <input className="w-full h-[30px] text-[#000000] p-2 focus:outline-none" />
+            <div className="flex justify-end pr-[6px]">
+              <button
+                type="button"
+                className="bg-[#0d9f16] h-[45px] w-[45px] rounded-full flex items-center justify-center"
+              >
+                <img
+                  src="https://cdn.leadstaker.com/icons/arrowheadRight128px.png"
+                  alt="Enviar"
+                  title="Enviar"
+                  className="h-[20px] invert"
+                />
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/compounded.tsx
+++ src/pages/compounded.tsx
@@ -1,17 +1,40 @@
+import logo from "../assets/logo.png";
+import background from "../assets/back.png";
+import imageMan from "../assets/man.png";
+import delivery from "../assets/delivery.png";
+import herbal from "../assets/herbal.png";
+import location from "../assets/location.png";
+import medicine from "../assets/medicine.png";
+import rate from "../assets/rate.png";
+import skincare from "../assets/skincare.png";
+import leaf from "../assets/leaf.png";
+import { useState } from "react";
+import FullChatModal from "../components/fullChatModal";
 
-import logo from '../assets/logo.png';
-import background from '../assets/back.png';
-import imageMan from '../assets/man.png';
-import delivery from '../assets/delivery.png';
-import herbal from '../assets/herbal.png';
-import location from '../assets/location.png';
-import medicine from '../assets/medicine.png';
-import rate from '../assets/rate.png';
-import skincare from '../assets/skincare.png';
-import leaf from '../assets/leaf.png';
-import { useState } from 'react';
-import FullChatModal from '../components/fullChatModal';
+// declare global {
+//   interface Window {
+//     LTK_DATA: any; // eslint-disable-line
+//   }
+// }
 
+// (function (l, d, s, t, k) {
+//   fetch(t + "?client=" + k, {
+//     headers: { href: l.location.href, p: l.performance.now().toString() },
+//   }).then(function (o) {
+//     o.json().then(function (o) {
+//       console.log(o);
+//       l.LTK_DATA = o;
+//       const c = d.createElement(s);
+//       d.body.appendChild(((c.innerHTML = o.js), c));
+//     });
+//   });
+// })(
+//   window,
+//   document,
+//   "script",
+//   "https://api.leadstaker.com/chats",
+//   "663ba7ef3644a70024d469fd"
+// );
 
 export default function Compounded() {
   const [open, setOpen] = useState(false);
@@ -23,10 +46,14 @@ export default function Compounded() {
         <div className="flex flex-1 justify-center p-4">
           <img src={logo} alt="logo" className="" />
         </div>
-        <div 
-          className="flex bg-[#F5F5F5] bg-no-repeat bg-cover h-[496px] z-0" 
-          style={{backgroundImage: `url(${background})`}}>
-          <div style={{background: 'rgba(18,40,15,0.74)'}} className="flex justify-center z-10 flex-1">
+        <div
+          className="flex bg-[#F5F5F5] bg-no-repeat bg-cover h-[496px] z-0"
+          style={{ backgroundImage: `url(${background})` }}
+        >
+          <div
+            style={{ background: "rgba(18,40,15,0.74)" }}
+            className="flex justify-center z-10 flex-1"
+          >
             <div className="flex items-center">
               <div className="flex flex-col">
                 <h1 className="text-5xl text-white font-bold pb-2">
@@ -35,17 +62,19 @@ export default function Compounded() {
                 <h1 className="text-5xl text-white font-bold">
                   Pede Amazônia Fórmula.
                 </h1>
-                <button 
+                <button
                   className="bg-[#18970c] font-bold text-lg h-[64px] mt-4 text-white p-[14px] w-[294px] rounded-[26px]"
-                  onClick={() => setOpen(true)}   
+                  onClick={() => setOpen(true)}
                 >
                   Faça seu manipulado agora!
                 </button>
               </div>
               <div className="flex flex-1 self-end">
-                <img src={imageMan} 
-                  alt="Imagem de um homem com uma caixa de remédio" 
-                  className="h-[480px]" />
+                <img
+                  src={imageMan}
+                  alt="Imagem de um homem com uma caixa de remédio"
+                  className="h-[480px]"
+                />
               </div>
             </div>
           </div>
@@ -54,24 +83,44 @@ export default function Compounded() {
           <div className="flex flex-col">
             <div className="grid grid-cols-3 w-[900px]">
               <div className="flex flex-col items-center w-[280px] transform transition duration-500 hover:scale-110">
-                <img src={skincare} alt="Imagem de produtos de skincare" className="mb-3"/>
-                <h3 className="text-2xl font-bold text-green-500 text-[21px]">Escolha pela eficácia</h3>
+                <img
+                  src={skincare}
+                  alt="Imagem de produtos de skincare"
+                  className="mb-3"
+                />
+                <h3 className="text-2xl font-bold text-green-500 text-[21px]">
+                  Escolha pela eficácia
+                </h3>
                 <p className="text-center text-gray-650">
-                  Insumos de fabricantes com melhores práticas e certificação de mercados internacionais.
+                  Insumos de fabricantes com melhores práticas e certificação de
+                  mercados internacionais.
                 </p>
               </div>
               <div className="flex flex-col items-center w-[280px] transform transition duration-500 hover:scale-110">
-                <img src={medicine} alt="Imagem de produtos de skincare" className="mb-3"/>
-                <h3 className="text-2xl font-bold text-green-500 text-[21px]">Use com confiança</h3>
+                <img
+                  src={medicine}
+                  alt="Imagem de produtos de skincare"
+                  className="mb-3"
+                />
+                <h3 className="text-2xl font-bold text-green-500 text-[21px]">
+                  Use com confiança
+                </h3>
                 <p className="text-center text-gray-650">
                   Profissionais competentes trabalhando em estrutura de ponta.
                 </p>
               </div>
               <div className="flex flex-col items-center w-[280px] transform transition duration-500 hover:scale-110">
-                <img src={delivery} alt="Imagem de produtos de skincare" className="mb-3"/>
-                <h3 className="text-2xl font-bold text-green-500 text-[21px]">Seu estoque sempre em dia</h3>
+                <img
+                  src={delivery}
+                  alt="Imagem de produtos de skincare"
+                  className="mb-3"
+                />
+                <h3 className="text-2xl font-bold text-green-500 text-[21px]">
+                  Seu estoque sempre em dia
+                </h3>
                 <p className="text-center text-gray-650">
-                  Entrega em toda a cidade, com opção de entrega ou retirada em loja.
+                  Entrega em toda a cidade, com opção de entrega ou retirada em
+                  loja.
                 </p>
               </div>
             </div>
@@ -81,22 +130,41 @@ export default function Compounded() {
           <div className="flex flex-col">
             <div className="grid grid-cols-3 w-[900px]">
               <div className="flex flex-col items-center w-[280px] transform transition duration-500 hover:scale-110">
-                <img src={location} alt="Imagem de produtos de skincare" className="mb-3"/>
-                <h3 className="text-2xl font-bold text-green-500 text-[21px]">Em toda Manaus</h3>
+                <img
+                  src={location}
+                  alt="Imagem de produtos de skincare"
+                  className="mb-3"
+                />
+                <h3 className="text-2xl font-bold text-green-500 text-[21px]">
+                  Em toda Manaus
+                </h3>
                 <p className="text-center text-gray-650">
                   Receba em sua casa ou em uma das nossas 11 lojas em Manaus.
                 </p>
               </div>
               <div className="flex flex-col items-center w-[280px] transform transition duration-500 hover:scale-110">
-                <img src={herbal} alt="Imagem de produtos de skincare" className="mb-3"/>
-                <h3 className="text-2xl font-bold text-green-500 text-[21px]">Mais que medicamentos</h3>
+                <img
+                  src={herbal}
+                  alt="Imagem de produtos de skincare"
+                  className="mb-3"
+                />
+                <h3 className="text-2xl font-bold text-green-500 text-[21px]">
+                  Mais que medicamentos
+                </h3>
                 <p className="text-center text-gray-650">
-                  Além de manipulados, compre vitaminas, homeopáticos ou florais.
+                  Além de manipulados, compre vitaminas, homeopáticos ou
+                  florais.
                 </p>
               </div>
               <div className="flex flex-col items-center w-[280px] transform transition duration-500 hover:scale-110">
-                <img src={rate} alt="Imagem de produtos de skincare" className="mb-3"/>
-                <h3 className="text-2xl font-bold text-green-500 text-[21px]">Fique tranquilo</h3>
+                <img
+                  src={rate}
+                  alt="Imagem de produtos de skincare"
+                  className="mb-3"
+                />
+                <h3 className="text-2xl font-bold text-green-500 text-[21px]">
+                  Fique tranquilo
+                </h3>
                 <p className="text-center text-gray-650">
                   São 20 anos de dedicação em excelência e qualidade.
                 </p>
@@ -104,24 +172,33 @@ export default function Compounded() {
             </div>
           </div>
         </div>
-        <div style={{background: 'rgba(52,99,59,0.95)', backgroundImage: `url(${leaf})`}} className="z-0">
-          <div style={{background: 'rgba(52,99,59,0.95)'}} className="z-10 py-20">
+        <div
+          style={{
+            background: "rgba(52,99,59,0.95)",
+            backgroundImage: `url(${leaf})`,
+          }}
+          className="z-0"
+        >
+          <div
+            style={{ background: "rgba(52,99,59,0.95)" }}
+            className="z-10 py-20"
+          >
             <h1 className="text-4xl text-white font-bold text-center mb-8">
-              Como funciona o Amazônia  <br/>
+              Como funciona o Amazônia <br />
               Fórmula Express?
             </h1>
             <div className="flex justify-center">
-              <div className="border-b-[5px] w-[54px] rounded-[3px] mb-6">
-              </div>
+              <div className="border-b-[5px] w-[54px] rounded-[3px] mb-6"></div>
             </div>
             <div className="flex justify-center">
               <p className="text-lg text-center w-[550px]">
-                Visando sua comodidade e conforto a Amazônia Fórmula oferece um serviço de Entrega em Domicílio. O medicamento chega no local de sua preferência, com toda a segurança e qualidade.
+                Visando sua comodidade e conforto a Amazônia Fórmula oferece um
+                serviço de Entrega em Domicílio. O medicamento chega no local de
+                sua preferência, com toda a segurança e qualidade.
               </p>
             </div>
             <div className="flex justify-center">
-              <button 
-                className="bg-[#18970c] font-bold text-xl h-[64px] mt-3 text-white p-[14px] w-[314px] rounded-[5px]">
+              <button className="bg-[#18970c] font-bold text-xl h-[64px] mt-3 text-white p-[14px] w-[314px] rounded-[5px]">
                 Faça seu manipulado agora!
               </button>
             </div>
@@ -130,8 +207,7 @@ export default function Compounded() {
         <div className="flex flex-1 justify-center p-4">
           <img src={logo} alt="logo" className="" />
         </div>
-     </div>
+      </div>
     </>
-  )
+  );
 }
-
